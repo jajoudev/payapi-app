12e08de07996ecbad90d26f2ae96b82b26f9ba4a
JajouDev <jahelgithub64@gmail.com>
2024-12-16 17:02:37 +0100

    app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+// Display burgerMenu
 const displayBurgerMenu = document.querySelector(".burger-container");
 const burgerMenuIcon = document.querySelector(".burger-btn");
-const closeMenuIcon = document.querySelector('.close-btn')
+const closeMenuIcon = document.querySelector(".close-btn");
 
 burgerMenuIcon.addEventListener("click", () => {
   if (displayBurgerMenu.classList.contains("hidden")) {
@@ -8,10 +9,32 @@ burgerMenuIcon.addEventListener("click", () => {
   }
 });
 
-closeMenuIcon.addEventListener('click', () => {
+closeMenuIcon.addEventListener("click", () => {
   if (displayBurgerMenu.classList.contains("burger-container")) {
     displayBurgerMenu.classList.add("hidden");
   }
-})
+});
 
+// ErrorMessage
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputEmail = document.querySelector(".email");
+  const errorMessage = document.querySelector(".error-message");
+  const btnSubmit = document.querySelector("button");
+
+  btnSubmit.addEventListener("click", () => {
+    if (btnSubmit.click) {
+      if (
+        inputEmail.value === "" &&
+        inputEmail.value === inputEmail.value.toUpperCase()
+      ) {
+        errorMessage.classList.remove("hidden");
+      } else if (inputEmail !== "") {
+        errorMessage.classList.add("hidden");
+      }
+    }
+  });
+});
